5f432c100a8024dd91aa0bd44c4a8869f3fa35db
ohalbig <o.halbig@googlemail.com>
2016-11-24 22:59:05 +0100

    New Assets
    Pull du Kacknoob

---
+++ Flying Penguin/Assets/Scripts/DrawACard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DeckOfCards : MonoBehaviour
+{
+    public GameObject[] cards;
+    public Transform cardSpawn = null;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+
+            GameObject card = cards[Random.Range(0, cards.Length)];
+            Instantiate(card);
+            card.transform.SetParent(this.transform.parent);
+        }
+    }
+}
---
+++ Flying Penguin/Assets/Scripts/DropArea.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropArea : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        CardDragger d = eventData.pointerDrag.GetComponent<CardDragger>();
+        if (d != null)
+        {
+            d.placeholderParent = this.transform;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        CardDragger d = eventData.pointerDrag.GetComponent<CardDragger>();
+        if (d != null && d.placeholderParent == this.transform)
+        {
+            d.placeholderParent = d.parentToReturnTo;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        CardDragger d = eventData.pointerDrag.GetComponent<CardDragger>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+
+}
